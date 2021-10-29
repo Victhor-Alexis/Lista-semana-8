7bc36c7621e331adeb72509e3866715794ad2a60
Victhor-Alexis <victhoralexis123@gmail.com>
2021-10-29 12:04:55 -0300

    Questão 1.

---
+++ questao1.js
@@ -0,0 +1,36 @@
+/* Alternativa a */
+
+const calculateIMC = (peso, altura) => peso / altura**2
+
+/* * * * */
+
+/* Alternativa b */
+
+const generateUser = (nome, idade, email) => ({
+    nome: nome,
+    idade: idade,
+    email: email
+})
+
+/* * * * */
+
+/* Alternativa c */
+
+let arr = [1,2,3,4,5,6,7,8,9,10] 
+
+//let soPar = arr.filter(num => num % 2 == 0)
+
+const soPar = arr => {
+    let arrPar = []
+
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] % 2 == 0)
+            arrPar.push(arr[i])
+    }
+
+    return arrPar
+}
+
+console.log(soPar(arr))
+
+/* * * * */
